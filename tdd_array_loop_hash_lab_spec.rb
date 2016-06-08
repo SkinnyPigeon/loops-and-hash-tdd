48aff8e4e5480b56583cfde18e1cab8607897ea9
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-08 15:20:18 +0100

    initial commit

---
+++ tdd_array_loop_hash_lab_spec.rb
@@ -0,0 +1,10 @@
+require('minitest/autorun')
+require('./tdd_array_loop_hash_lab')
+
+class Lab < MiniTest::Test
+  # write your tests in here
+
+  # to run your tests: 
+  # ruby tdd_array_hash_lab_spec.rb
+
+end
